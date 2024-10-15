d2d2717c7b3a779b52a968e5f79e7c4e9f78e7dd
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-10-15 18:26:35 +0300

    feat(sS2bb8TA): add login method
    The login method in PersonService
    returns API key by user credentials.

--- backend/src/main/java/edu/architect_711/words/service/PersonService.java
+++ backend/src/main/java/edu/architect_711/words/service/PersonService.java
@@ -1,5 +1,8 @@
 package edu.architect_711.words.service;
 
+import edu.architect_711.words.model.entity.Person;
+import edu.architect_711.words.repository.AuthoritiesRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +14,23 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class PersonService {
+    @Value("${api.security.key.title:x-api-key}")
+    private String apiKeyTitle;
+
     private final PersonRepository personRepository;
+    private final AuthoritiesRepository authoritiesRepository;
 
     public ResponseEntity<PersonDto> getPrimaryData(final String HEADER_API_KEY) {
         return ResponseEntity.ok().body(PersonMapper.toDto(personRepository.findPersonByApiKey(HEADER_API_KEY).orElseThrow()));
     }
 
+    public ResponseEntity<?> login(final PersonDto personDto) {
+        final Person person = personRepository.findByUsername(personDto.getUsername()).orElseThrow();
+
+        if (!person.getPassword().equals(personDto.getPassword()))
+            throw new IllegalArgumentException("Incorrect username or password");
+        final String FOUND_API_KEY = authoritiesRepository.findApiKeyByUserId(person.getId()).orElseThrow();
+
+        return ResponseEntity.ok().header(apiKeyTitle, FOUND_API_KEY).build();
+    }
 }
