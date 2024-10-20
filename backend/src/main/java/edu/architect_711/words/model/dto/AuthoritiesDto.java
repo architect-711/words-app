55fd855ab55e2dc3174c4df3f8490a3fab948827
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-10-20 13:45:42 +0300

    refactor(74qFSWXi): remove unused import!!!
    Intellij for some reason saved another changes
    on prev such commit.

--- backend/src/main/java/edu/architect_711/words/model/dto/AuthoritiesDto.java
+++ backend/src/main/java/edu/architect_711/words/model/dto/AuthoritiesDto.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data @AllArgsConstructor @NoArgsConstructor
 public class AuthoritiesDto {
     private Long userId;
