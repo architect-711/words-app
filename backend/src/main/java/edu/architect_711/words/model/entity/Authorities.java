a53d832ec8d269afd680e007dfd96b65d26c3404
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-10-20 13:42:23 +0300

    feat(74qFSWXi): make authorities work.
    There were problems with enum field.
    Since Postgres is ORM it waits only for
    objects, but Hibernate doesn't know that
    and send VARCHAR to enum field.
    
    Via new dependency (in build.gradle.kts)
    I tell to Hibernate how it should map
    this fields to Postgres table.
    
    I was solving this issue for 4 days.

--- backend/src/main/java/edu/architect_711/words/model/entity/Authorities.java
+++ backend/src/main/java/edu/architect_711/words/model/entity/Authorities.java
@@ -2,22 +2,43 @@ package edu.architect_711.words.model.entity;
 
 import edu.architect_711.words.model.dto.PersonAuthorities;
 import edu.architect_711.words.model.dto.PersonRole;
+import io.hypersistence.utils.hibernate.type.array.EnumArrayType;
+import io.hypersistence.utils.hibernate.type.array.internal.AbstractArrayType;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.Parameter;
+import org.hibernate.type.SqlTypes;
 
 @NoArgsConstructor
-@Data @Entity @Table(name = "authorities")
+@Data @Entity
+@Table(name = "authorities")
 public class Authorities {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "user_id", nullable = false)
     private Long userId;
+
     @Column(name = "api_key", nullable = false)
     private String api_key;
-    @Column(nullable = false)
+
+    @Column(nullable = false, name = "authorities", columnDefinition = "user_authorities[]")
+    @Type(
+            value = EnumArrayType.class,
+            parameters = @Parameter(
+                    name = AbstractArrayType.SQL_ARRAY_TYPE,
+                    value = "user_authorities"
+            )
+    )
     private PersonAuthorities[] authorities;
-    @Column(nullable = false) @Enumerated(EnumType.STRING)
+
+    @Column(nullable = false, name = "role", columnDefinition = "user_role")
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Enumerated(EnumType.STRING)
     private PersonRole role;
 
     public Authorities(Long userId, String api_key, PersonAuthorities[] authorities, PersonRole role) {
