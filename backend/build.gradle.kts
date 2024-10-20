df73a5488209d5415d0466373ae0f0afabf807c1
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-10-20 13:36:20 +0300

    build(74qFSWXi): add dependency for enum saving

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.8.3") // Postgres is the ORM database, and can't accept enums/array of enums pure, it helps.
 
 	compileOnly("org.projectlombok:lombok")
 
