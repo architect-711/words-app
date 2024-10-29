99065f73eedd608003846d774486617678c45bfe
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-10-29 19:30:21 +0300

    feat(brIBMr6C): add db filler

--- backend/src/main/java/edu/architect_711/words/startup/DatabaseInitFiller.java
+++ backend/src/main/java/edu/architect_711/words/startup/DatabaseInitFiller.java
@@ -1,11 +1,17 @@
 package edu.architect_711.words.startup;
 
 import edu.architect_711.words.model.dto.PersonAuthorities;
+import edu.architect_711.words.model.dto.PersonDto;
 import edu.architect_711.words.model.dto.PersonRole;
+import edu.architect_711.words.model.dto.WordLanguage;
 import edu.architect_711.words.model.entity.Authorities;
 import edu.architect_711.words.model.entity.Person;
+import edu.architect_711.words.model.entity.Word;
+import edu.architect_711.words.model.mapper.PersonMapper;
 import edu.architect_711.words.repository.AuthoritiesRepository;
 import edu.architect_711.words.repository.PersonRepository;
+import edu.architect_711.words.repository.WordsRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,41 +20,69 @@ import java.util.List;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-@Component @RequiredArgsConstructor @Profile("dev") @Slf4j
+@Component @RequiredArgsConstructor @Profile("dev") @Slf4j @Transactional
 public class DatabaseInitFiller implements ApplicationRunner {
     private final PersonRepository personRepository;
     private final AuthoritiesRepository authoritiesRepository;
+    private final WordsRepository wordsRepository;
 
-    private static final List<Person> TEST_PEOPLE = List.of(
-        new Person("test1", "1234"),
-        new Person("test2", "password")
+    private static final List<PersonDto> TEST_PEOPLE = List.of(
+        new PersonDto("test1", "1234"),
+        new PersonDto("test2", "password")
     );
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         fillDatabase();
-        
-        log.info("Saved in database values: ");
-        personRepository.findAll().forEach(person -> log.info(person.toString()));
-        authoritiesRepository.findAll().forEach(a -> log.info(a.toString()));
-        log.info("Test database entities saved.");
+
+        log.info("Test database entities saved: ");
+        List.of(wordsRepository, personRepository, authoritiesRepository).forEach(this::logSavedData); 
+    }
+
+    private void logSavedData(final JpaRepository<?, Long> repository) {
+        repository.findAll().forEach(data -> log.info("--- Saved entity: {}", data));
     }
 
     private void fillDatabase() {
-        for (final Person person : TEST_PEOPLE) {
-            final Long createdPersonId = personRepository.save(person).getId();
-            final Authorities preparedAuthority = new Authorities(
-                    createdPersonId,
-                    person.getUsername(),
-                    new PersonAuthorities[] {PersonAuthorities.READ, PersonAuthorities.DELETE, PersonAuthorities.UPDATE},
-                    PersonRole.USER
-            );
+        for (final PersonDto person : TEST_PEOPLE) {
+            final Person savedPerson = savePerson(person);
 
-            log.info("Saving authority: {}", preparedAuthority);
-
-            authoritiesRepository.save(preparedAuthority);
+            saveAuthorities(savedPerson.getId());
+            saveWord(savedPerson.getId());
         }
+    }
+
+    private Person savePerson(final PersonDto personDto) {
+        return personRepository.save(PersonMapper.toPerson(personDto));
+    }
+
+    private Authorities saveAuthorities(final Long id) {
+        final Person savedPerson = getReferencedPersonById(id); 
+
+        return authoritiesRepository.save(new Authorities(
+            savedPerson, 
+            savedPerson.getUsername(), 
+            new PersonAuthorities[] {PersonAuthorities.READ, PersonAuthorities.DELETE, PersonAuthorities.UPDATE},
+            PersonRole.USER
+        ));  
+    }
+
+    private Word saveWord(final Long id) {
+        final Person savedPerson = getReferencedPersonById(id); 
+
+        final Word wordToBeSaved = new Word(
+            savedPerson,
+            "test_word_" + savedPerson.getUsername(),
+            WordLanguage.ENGLISH
+        );
+
+        return wordsRepository.save(wordToBeSaved);
+    }
+
+    private Person getReferencedPersonById(final Long id) {
+        return personRepository.getReferenceById(id);
     }
 }
