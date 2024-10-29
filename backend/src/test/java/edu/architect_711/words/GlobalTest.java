cdd7ebd6c37b8e2dbc5bd4823750c30589c576b5
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-10-29 19:36:47 +0300

    test(brIBMr6C): add main test
    It tests the context loads.

---
+++ backend/src/test/java/edu/architect_711/words/GlobalTest.java
@@ -0,0 +1,10 @@
+package edu.architect_711.words;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GlobalTest {
+    @Test
+    public void contextLoads() {}
+}
