39471a47786f9d33ed0d5d38112aa594d926e085
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-10-29 19:28:45 +0300

    feat(brIBMr6C): fix infernal entities
    There are some errors because of enum
    usage and many-to-one relationships
    
    Also fix them DTOs

--- backend/src/main/java/edu/architect_711/words/model/dto/AuthoritiesDto.java
+++ backend/src/main/java/edu/architect_711/words/model/dto/AuthoritiesDto.java
@@ -1,12 +1,13 @@
 package edu.architect_711.words.model.dto;
 
+import edu.architect_711.words.model.entity.Person;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @AllArgsConstructor @NoArgsConstructor
 public class AuthoritiesDto {
-    private Long userId;
+    private Person userId;
     private String apiKey;
     private PersonAuthorities[] authorities;
     private PersonRole role;
