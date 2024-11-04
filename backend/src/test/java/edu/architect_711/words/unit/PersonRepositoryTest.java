2d749a9991b3408fc9066899a7df8b8d826714d9
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-11-04 19:10:41 +0300

    refactor(brIBMr6C): clean test class

--- backend/src/test/java/edu/architect_711/words/unit/PersonRepositoryTest.java
+++ backend/src/test/java/edu/architect_711/words/unit/PersonRepositoryTest.java
@@ -10,8 +10,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
-import java.util.Optional;
 
+import static edu.architect_711.words.model.mapper.PersonMapper.fromListToPerson;
+import static edu.architect_711.words.unit.UnitTestEntitiesConfiguration.getTestPeople;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
@@ -21,11 +22,6 @@ public class PersonRepositoryTest {
     @Autowired
     private PersonRepository personRepository;
 
-    private static final List<Person> testPeople = List.of(
-            new Person("username1", "password"),
-            new Person("username2", "password")
-    );
-
     @BeforeEach
     public void setUp() {
         personRepository.deleteAll();
@@ -33,7 +29,7 @@ public class PersonRepositoryTest {
 
     @Test
     public void saveAll() {
-        List<Person> savedPeople = personRepository.saveAll(testPeople);
+        List<Person> savedPeople = personRepository.saveAll(getMappedTestPeople());
 
         assertThat(savedPeople).isNotEmpty();
         assertThat(savedPeople).isNotNull();
@@ -44,33 +40,33 @@ public class PersonRepositoryTest {
 
     @Test
     public void deleteOne() {
-        final Person savedPerson = save(testPeople.getFirst());
+        final Person savedPerson = saveFirst();
 
         personRepository.deleteById(savedPerson.getId());
-        final Person deletedPerson = personRepository.findByUsername(testPeople.getFirst().getUsername()).orElse(null);
+        final Person deletedPerson = personRepository.findByUsername(savedPerson.getUsername()).orElse(null);
 
         assertThat(deletedPerson).isNull();
     }
 
     @Test
     public void updateOne() {
-        final Person savedPerson = save(testPeople.getFirst());
+        final String NEW_WORD_PASSWORD = "1234";
+        final Person savedPerson = saveFirst();
 
         assertThat(personRepository.findByUsername(savedPerson.getUsername())).isPresent();
 
-        savedPerson.setPassword("1234");
-
+        savedPerson.setPassword(NEW_WORD_PASSWORD);
         personRepository.save(savedPerson);
 
-        final Optional<Person> foundUpdatedByIdOptional = personRepository.findById(savedPerson.getId());
-        assertThat(foundUpdatedByIdOptional).isPresent();
-
-        final Person foundUpdatedById = foundUpdatedByIdOptional.get();
-        assertThat(foundUpdatedById.getPassword()).isEqualTo("1234");
-
+        final Person foundUpdatedById = personRepository.findById(savedPerson.getId()).orElseThrow();
+        assertThat(foundUpdatedById.getPassword()).isEqualTo(NEW_WORD_PASSWORD);
     }
 
-    private Person save(final Person person) {
-        return personRepository.save(person);
+    private Person saveFirst() {
+        return personRepository.save(getMappedTestPeople().getFirst());
+    }
+
+    private List<Person> getMappedTestPeople() {
+        return fromListToPerson(getTestPeople());
     }
 }
