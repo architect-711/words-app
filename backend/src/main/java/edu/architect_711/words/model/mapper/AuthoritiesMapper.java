1ea3dce7986318e78d30a73857bb11401110a5c2
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-11-05 21:38:43 +0300

    refactor(brIBMr6C): update mapper

--- backend/src/main/java/edu/architect_711/words/model/mapper/AuthoritiesMapper.java
+++ backend/src/main/java/edu/architect_711/words/model/mapper/AuthoritiesMapper.java
@@ -2,13 +2,14 @@ package edu.architect_711.words.model.mapper;
 
 import edu.architect_711.words.model.dto.AuthoritiesDto;
 import edu.architect_711.words.model.entity.Authorities;
+import edu.architect_711.words.model.entity.Person;
 
 public class AuthoritiesMapper {
     public static AuthoritiesDto toDto(final Authorities authorities) {
-        return new AuthoritiesDto(authorities.getUserId(), authorities.getApi_key(), authorities.getAuthorities(), authorities.getRole());
+        return new AuthoritiesDto(authorities.getPerson().getId(), authorities.getApi_key(), authorities.getAuthorities(), authorities.getRole());
     } 
 
-    public static Authorities toEntity(final AuthoritiesDto dto) {
-        return new Authorities(dto.getUserId(), dto.getApiKey(), dto.getAuthorities(), dto.getRole());
+    public static Authorities toEntity(final AuthoritiesDto dto, final Person person) {
+        return new Authorities(person, dto.getApiKey(), dto.getAuthorities(), dto.getRole());
     }
 }
