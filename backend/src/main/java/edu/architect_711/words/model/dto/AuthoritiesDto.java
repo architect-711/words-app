50f5bb297be9f4d0ee4ce4efedcdda4d4e5dd987
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-11-05 21:38:29 +0300

    refactor(brIBMr6C): change field type

--- backend/src/main/java/edu/architect_711/words/model/dto/AuthoritiesDto.java
+++ backend/src/main/java/edu/architect_711/words/model/dto/AuthoritiesDto.java
@@ -1,13 +1,12 @@
 package edu.architect_711.words.model.dto;
 
-import edu.architect_711.words.model.entity.Person;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @AllArgsConstructor @NoArgsConstructor
 public class AuthoritiesDto {
-    private Person userId;
+    private Long userId;
     private String apiKey;
     private PersonAuthorities[] authorities;
     private PersonRole role;
