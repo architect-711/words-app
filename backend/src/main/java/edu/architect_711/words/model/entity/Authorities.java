e19309e6b23d879c4ed04f612c21195892c72707
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-11-05 21:38:08 +0300

    refactor(brIBMr6C): clean authorities
    Rename field userId to person, since
    because of @JoinColumn the field gets the joined
    column type.
    
    @JsonIgnore since then the entity is returned
    this field isn't required. And BTW it shouldn't
    be returned, there is a dto

--- backend/src/main/java/edu/architect_711/words/model/entity/Authorities.java
+++ backend/src/main/java/edu/architect_711/words/model/entity/Authorities.java
@@ -1,5 +1,6 @@
 package edu.architect_711.words.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import edu.architect_711.words.model.dto.PersonAuthorities;
 import edu.architect_711.words.model.dto.PersonRole;
 import io.hypersistence.utils.hibernate.type.array.EnumArrayType;
@@ -22,8 +23,10 @@ public class Authorities {
 
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false, unique = true)
-    private Person userId;
+    @JsonIgnore
+    private Person person;
 
+    @Column(name = "api_key", length = 128, nullable = false, unique = true)
     private String api_key; 
 
     @Column(nullable = false, name = "authorities", columnDefinition = "user_authorities[]")
@@ -41,8 +44,8 @@ public class Authorities {
     @Enumerated(EnumType.STRING)
     private PersonRole role;
 
-    public Authorities(Person userId, String api_key, PersonAuthorities[] authorities, PersonRole role) {
-        this.userId = userId;
+    public Authorities(Person person, String api_key, PersonAuthorities[] authorities, PersonRole role) {
+        this.person = person;
         this.api_key = api_key;
         this.authorities = authorities;
         this.role = role;
