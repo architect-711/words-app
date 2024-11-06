b5c5ca229ef19a84108e7d6eb1e8cf66a7edcabd
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-11-06 18:33:14 +0300

    refactor(brIBMr6C): change names and add required comment

--- backend/src/test/java/edu/architect_711/words/unit/configuration/UnitTestEntitiesConfiguration.java
+++ backend/src/test/java/edu/architect_711/words/unit/configuration/UnitTestEntitiesConfiguration.java
@@ -4,15 +4,19 @@ import edu.architect_711.words.model.dto.*;
 
 import java.util.List;
 
+/**
+ * Below entities are intended ONLY for testing and should never be used in production. Also
+ * remember that the amount of entities MUST be the same
+ * */
 public class UnitTestEntitiesConfiguration {
-    public static List<PersonDto> getTestPeople() {
+    public static List<PersonDto> getTestPeopleDTOs() {
         return List.of(
                 new PersonDto("username1", "password"),
                 new PersonDto("username2", "password")
         );
     }
 
-    public static List<WordDto> getTestWords() {
+    public static List<WordDto> getTestWordsDTOs() {
         return List.of(
                 new WordDto(null, null, "test1", WordLanguage.ENGLISH),
                 new WordDto(null, null, "test2", WordLanguage.ENGLISH)
