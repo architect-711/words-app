c1dc4a262df30dc610b9b37a1379ea88409ffb5a
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-12-09 16:31:08 +0300

    build!(bhDkZWh0): update build logic
    Explain later

--- backend/src/main/java/edu/architect_711/words/startup/EnvironmentVariablesLoader.java
+++ backend/src/main/java/edu/architect_711/words/startup/EnvironmentVariablesLoader.java
@@ -13,8 +13,8 @@ import java.util.stream.Stream;
 
 @Component
 public class EnvironmentVariablesLoader implements ApplicationListener<ApplicationStartingEvent> {
-    private final static String DEFAULT_ENV_FILE_NAME = ".env";
-    private final static String DEFAULT_ENV_FILE_LOCATION = ".."; // upper folder
+    private final static String DEFAULT_ENV_FILE_NAME= ".env";
+    private final static String UPPER_FOLDER = ".."; // upper folder
 
     @Override
     public void onApplicationEvent(ApplicationStartingEvent event) {
@@ -22,16 +22,16 @@ public class EnvironmentVariablesLoader implements ApplicationListener<Applicati
     }
 
     public void loadEnvVarsToSystem() {
-        loadEnvVars(this::loadVar);
+        loadEnvVars(this::loadVarToSystem);
     }
 
     /**
      * You may need to either to System or to Environment, so decide for yourself.
      * */
     public void loadEnvVars(final Consumer<String> setter) {
-        try (final Stream<String> lines = Files.lines(Paths.get(getEnvPath().toString()))) {
+        try (Stream<String> lines = Files.lines(Paths.get(getEnvPath().toString()))) {
             lines.forEach(setter);
-        } catch (final Exception exception) {
+        } catch (Exception exception) {
             throw new IllegalStateException("Couldn't load all env vars:/");
         }
     }
@@ -43,13 +43,13 @@ public class EnvironmentVariablesLoader implements ApplicationListener<Applicati
      * isn't the same as if you would do the same from terminal (the backend folder where ./gradlew locates)
      * */
     private Path getEnvPath() {
-        return new File(DEFAULT_ENV_FILE_NAME).exists() ? Paths.get(DEFAULT_ENV_FILE_NAME) : Paths.get(DEFAULT_ENV_FILE_LOCATION, DEFAULT_ENV_FILE_NAME);
+        return new File(DEFAULT_ENV_FILE_NAME).exists() ? Paths.get(DEFAULT_ENV_FILE_NAME) : Paths.get(UPPER_FOLDER, DEFAULT_ENV_FILE_NAME);
     }
 
-    public void loadVar(final String ENV_FILE_LINE) {
-        if (ENV_FILE_LINE.trim().isEmpty() || ENV_FILE_LINE.startsWith("#"))
+    public void loadVarToSystem(String line) {
+        if (line.isBlank() || line.startsWith("#"))
             return;
-        final String[] entry = ENV_FILE_LINE.split("=");
+        String[] entry = line.split("=");
 
         System.setProperty(entry[0], entry[1]);
     }
