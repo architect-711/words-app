26e8622a7410b0bd45466b671fd3b86ca45ff3c4
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-12-28 15:30:24 +0300

    build(UQcFOEQi): delete env vars loader

--- backend/src/main/java/edu/architect_711/words/WordsApplication.java
+++ backend/src/main/java/edu/architect_711/words/WordsApplication.java
@@ -1,12 +1,11 @@
 package edu.architect_711.words;
 
-import edu.architect_711.words.startup.EnvironmentVariablesLoader;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class WordsApplication {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(WordsApplication.class).listeners(new EnvironmentVariablesLoader()).run(args);
+		SpringApplication.run(WordsApplication.class, args);
 	}
 }
