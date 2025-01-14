c08589e95e1afad50679837bc6d0315dd95cfcc9
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2025-01-14 21:51:51 +0300

    fix!(78LXngTH): make WordServiceTest integration test
    Also add comments to tests
    AND rewrite some of them

--- backend/src/test/java/edu/architect_711/words/unit/service/WordServiceTest.java
+++ backend/src/test/java/edu/architect_711/words/unit/service/WordServiceTest.java
@@ -2,6 +2,9 @@ package edu.architect_711.words.unit.service;
 
 import edu.architect_711.words.model.dto.PersonDto;
 import edu.architect_711.words.model.dto.WordDto;
+import edu.architect_711.words.model.entity.Person;
+import edu.architect_711.words.model.mapper.PersonMapper;
+import edu.architect_711.words.repository.PersonRepository;
 import edu.architect_711.words.service.PersonService;
 import edu.architect_711.words.service.WordService;
 import jakarta.transaction.Transactional;
@@ -9,83 +12,81 @@ import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
-import java.util.Random;
+import java.util.Date;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Profile("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class WordServiceTest {
+public class WordServiceTest implements PersonMapper {
     @Autowired private WordService wordService;
     @Autowired private PersonService personService;
 
-    private static final LocalDateTime TEST_LOCAL_DATE_TIME = LocalDateTime.now();
+    @Autowired private PersonRepository personRepository;
 
+    private Person savedPersonOnStartup;
+
+    /**
+     * Get Random person or create new one
+     * */
     @BeforeAll
     public void setup() {
-        personService.create(new PersonDto(
-                String.valueOf(generateRandomInt()),
-                "password")
-        );
+        savedPersonOnStartup = personRepository
+                .findFirst()
+                .orElseGet(() -> personDtoToEntity(Objects.requireNonNull(
+                        personService.create(new PersonDto(
+                                new Date().toString(),
+                                "password")).getBody()
+                )));
     }
 
     /**
-     * On `create` service method, it is expected that object,
-     * sent by  client, hasn't got an `id` and `localDateTime` fields -
-     * because it is assigned by JPA and service
+     * On `create` service method, the `id` and `userId` fields
      * */
     @Test
     public void should_fail_creation() {
-        assertThrows(ConstraintViolationException.class, () -> wordService.create(buildDto(1L, null, null)));
-        assertThrows(ConstraintViolationException.class, () -> wordService.create(buildDto(null, TEST_LOCAL_DATE_TIME, null)));
+        assertThrows(ConstraintViolationException.class, () -> wordService.create(buildDto(savedPersonOnStartup.getId(), null, null)));
+        assertThrows(ConstraintViolationException.class, () -> wordService.create(buildDto(null, LocalDateTime.now(), null)));
     }
 
     /**
-     * On `update` service method the only disallowed fields
-     * are `userId` and `localDateTime` since they are never updated
+     * On `update` service method all fields must be not blank
      * */
     @Test
     public void should_fail_update() {
-        assertThrows(ConstraintViolationException.class, () -> wordService.create(buildDto(1L, TEST_LOCAL_DATE_TIME, null)));
-        assertThrows(ConstraintViolationException.class, () -> wordService.create(buildDto(1L, null, 1L)));
+        assertThrows(ConstraintViolationException.class, () -> wordService.update(new WordDto(null, null, " ", " ", " ", null, null)));
     }
 
     /**
      * <ul>
-     *     <li>For some reason `@Transactional` it saves from InvalidDataAccessApiUsageException</li>
+     *     <li>For some reason `@Transactional` saves from InvalidDataAccessApiUsageException</li>
      *     <li>For some reason even thought it successfully saves entity, it doesn't appear in the database (!)</li>
      * </ul>
      * */
     @Test
     @Transactional
     public void should_save() {
-        assertDoesNotThrow(() -> wordService.create(buildDto(null, null, 1L)));
+        assertDoesNotThrow(() -> wordService.create(buildDto(null, null, savedPersonOnStartup.getId())));
     }
 
     private static WordDto buildDto(Long id, LocalDateTime localDateTime, Long userId) {
         return new WordDto(
                 id,
                 userId,
-                String.valueOf(generateRandomInt()),
+                new Date().toString(),
                 "translation",
                 "description",
                 "German",
                 localDateTime
         );
-    }
-
-    private static int generateRandomInt() {
-        return new Random().nextInt(1_000_000) >> 2;
     }
 
 }
