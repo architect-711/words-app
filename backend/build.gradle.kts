be710e791a7c27660a187dfc42cd74362ade2155
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2025-01-25 21:40:49 +0300

    chore(DhmVXPFI): add devtools dependency

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -32,6 +32,9 @@ dependencies {
 	// https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
 
+	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-devtools
+	implementation("org.springframework.boot:spring-boot-devtools:3.4.1") // enhance development <speed
+
 	// https://mvnrepository.com/artifact/org.flywaydb/flyway-database-postgresql
 	runtimeOnly("org.flywaydb:flyway-database-postgresql:11.1.0") // db migration tool
 
