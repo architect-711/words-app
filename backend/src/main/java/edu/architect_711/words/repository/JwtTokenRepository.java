853434066ca82189bfcf3c0397ff874ebd9f1b6d
Dmitrii <jfjjjnqhqiriykkn@gmail.com>
2025-03-25 11:55:56 +0300

    fix(DhmVXPFI): remake disabling tokens

--- backend/src/main/java/edu/architect_711/words/repository/JwtTokenRepository.java
+++ backend/src/main/java/edu/architect_711/words/repository/JwtTokenRepository.java
@@ -1,29 +1,21 @@
 package edu.architect_711.words.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import edu.architect_711.words.entities.db.JwtTokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface JwtTokenRepository extends JpaRepository<JwtTokenEntity, Long> {
-    @Query(nativeQuery = true, value = """
-            WITH token AS (
-                SELECT * FROM token
-            )
-            UPDATE token
-            SET is_active = false
-            WHERE person_id = :id;
-            """)
-    void deactivateAllByPersonId(Long id);
-
+    @Modifying // because https://stackoverflow.com/a/58037702/22276970
     @Query(nativeQuery = true, value = """
             DELETE FROM token USING account
             WHERE token.account_id = account.id
             AND account.username = :username;
-                            """)
-    void deleteAllByUsername(String username);
+            """)
+    void deleteAllByUsername(@Param("username") String username);
 
     Optional<JwtTokenEntity> findByRefreshToken(String refreshToken);
 
