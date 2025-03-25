a5ebc351617b3c5a76f5dbd37088bf48f5b5c47a
Dmitrii <jfjjjnqhqiriykkn@gmail.com>
2025-03-26 00:19:09 +0300

    fix(DhmVXPFI): enable word controller

--- backend/src/main/java/edu/architect_711/words/controller/WordController.java
+++ backend/src/main/java/edu/architect_711/words/controller/WordController.java
@@ -1,33 +1,41 @@
 package edu.architect_711.words.controller;
 
-//@RestController
-//@RequestMapping("/words")
-//@RequiredArgsConstructor
+import edu.architect_711.words.entities.dto.WordDto;
+import edu.architect_711.words.service.word.DefaultWordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/words")
+@RequiredArgsConstructor
 public class WordController {
-//    private final WordService wordService;
-//
-//    @GetMapping
-//    public ResponseEntity<List<WordDto>> read(
-//            @RequestParam(name = "size", defaultValue = "5") Integer size,
-//            @RequestParam(name = "page", defaultValue = "0") Integer page
-//    ) {
-//        return wordService.read(size, page);
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<WordDto> create(@RequestBody WordDto wordDto) {
-//        return wordService.create(wordDto);
-//    }
-//
-//    @PutMapping
-//    public ResponseEntity<WordDto> update(@RequestBody WordDto wordDto) {
-//        return wordService.update(wordDto);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
-//        return wordService.delete(id);
-//    }
+    private final DefaultWordService defaultWordService;
+
+    @GetMapping
+    public ResponseEntity<List<WordDto>> read(
+            @RequestParam(name = "size", defaultValue = "5") Integer size,
+            @RequestParam(name = "page", defaultValue = "0") Integer page
+    ) {
+        return defaultWordService.read(size, page);
+    }
+
+    @PostMapping
+    public ResponseEntity<WordDto> create(@RequestBody WordDto wordDto) {
+        return defaultWordService.create(wordDto);
+    }
+
+    @PutMapping
+    public ResponseEntity<WordDto> update(@RequestBody WordDto wordDto) {
+        return defaultWordService.update(wordDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        return defaultWordService.delete(id);
+    }
     
 }
 
