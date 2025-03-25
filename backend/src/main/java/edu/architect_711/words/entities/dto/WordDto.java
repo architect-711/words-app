972c71b64beb86706adee58a4b8ee51b0326d8bb
Dmitrii <jfjjjnqhqiriykkn@gmail.com>
2025-03-26 00:20:56 +0300

    fix(DhmVXPFI): add default validation groups

--- backend/src/main/java/edu/architect_711/words/entities/dto/WordDto.java
+++ backend/src/main/java/edu/architect_711/words/entities/dto/WordDto.java
@@ -1,10 +1,10 @@
 package edu.architect_711.words.entities.dto;
 
 import edu.architect_711.words.entities.Word;
-import edu.architect_711.words.entities.validation_groups.WordValidationGroups;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
+import jakarta.validation.groups.Default;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,26 +13,29 @@ import java.time.LocalDateTime;
 
 @Data @AllArgsConstructor @NoArgsConstructor
 public class WordDto implements Word  {
-    @Null(message = "Id must be null.", groups = WordValidationGroups.Create.class)
-    @NotNull(message = "Id can't be null")
+    @Null(message = "Id must be null.", groups = OnCreate.class)
+    @NotNull(message = "Id can't be null", groups = OnCreated.class)
     private Long id;
 
-    @NotNull(message = "User id can't be null")
+    @NotNull(message = "User id can't be null", groups = Default.class)
     private Long userId;
 
-    @NotBlank(message = "Title cannot be blank")
+    @NotBlank(message = "Title cannot be blank", groups = Default.class)
     private String title;
 
-    @NotBlank(message = "WordEntity translation cannot be blank.")
+    @NotBlank(message = "WordEntity translation cannot be blank.", groups = Default.class)
     private String translation;
 
-    @NotBlank(message = "WordEntity description cannot be blank.")
+    @NotBlank(message = "WordEntity description cannot be blank.", groups = Default.class)
     private String description;
 
-    @NotBlank(message = "LanguageEntity cannot be blank")
+    @NotBlank(message = "LanguageEntity cannot be blank", groups = Default.class)
     private String language;
 
-    @Null(message = "Local date time must be null", groups = WordValidationGroups.Create.class)
-    @NotNull(message = "Time created cannot be blank")
+    @Null(message = "Local date time must be null", groups = OnCreate.class)
+    @NotNull(message = "Time created cannot be blank", groups = OnCreated.class)
     private LocalDateTime localDateTime;
+
+    public interface OnCreate {}
+    public interface OnCreated {}
 }
