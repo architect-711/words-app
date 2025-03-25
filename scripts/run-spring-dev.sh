453c63e5526ab9b19122c29070867cffd16fbc6f
Dmitrii <jfjjjnqhqiriykkn@gmail.com>
2025-03-25 11:52:03 +0300

    fix(DhmVXPFI): remove env loading, add clean

--- scripts/run-spring-dev.sh
+++ scripts/run-spring-dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export $(grep -v '^#' .env | xargs)
-
 cd backend || exit
 
-docker compose up -d postgres_local_db && ./gradlew bootRun --args='--spring.profiles.active=dev,postgres'
+./gradlew clean
+
+docker compose up -d postgres_local_db && ./gradlew bootRun --args='--spring.profiles.active=dev,postgres' 
