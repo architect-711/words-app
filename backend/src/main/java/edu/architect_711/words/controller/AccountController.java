faf9169c430b8bcde73abc7e5ff71d84a2498bde
Dmitrii <jfjjjnqhqiriykkn@gmail.com>
2025-03-25 11:56:19 +0300

    refactor(DhmVXPFI): update controller

--- backend/src/main/java/edu/architect_711/words/controller/AccountController.java
+++ backend/src/main/java/edu/architect_711/words/controller/AccountController.java
@@ -1,18 +1,16 @@
 package edu.architect_711.words.controller;
 
+import edu.architect_711.words.controller.service.AccountService;
 import edu.architect_711.words.entities.dto.AccountDto;
 import edu.architect_711.words.entities.dto.JwtTokenDto;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import edu.architect_711.words.controller.service.AccountService;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,11 +34,8 @@ public class AccountController {
     }
 
     @GetMapping("/logout")
-    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response,
-            Authentication authentication) {
-        accountService.logout(request, response, authentication);
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> logout(HttpServletRequest request) {
+        return accountService.logout(request);
     }
 
 }
