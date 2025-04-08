962e026cebd0b7eefefd10f16592cdba13f94837
Dmitrii <jfjjjnqhqiriykkn@gmail.com>
2025-04-08 20:02:14 +0300

    build(DhmVXPFI): add profile specific sh scripts

--- scripts/run-spring-dev.sh
+++ scripts/run-spring-dev.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-cd backend || exit
+source utils.sh
+source_dev
 
-./gradlew clean
+cd ../backend || exit
 
-docker compose up -d postgres_local_db && ./gradlew bootRun --args='--spring.profiles.active=dev,postgres' 
+docker compose up -d postgres_local_db
+
+if [ "$1" = "container" ]; then
+  ./gradlew clean build -x test
+
+  # overwrite JDBC url, since working in container
+  export DATABASE_URL="${DATABASE_URL/localhost:5434/"postgres_local_db:5432"}"
+
+  docker compose up --build --remove-orphans -d spring_backend
+else
+  ./gradlew bootRun --args='--spring.profiles.active=dev,postgres'
+fi
