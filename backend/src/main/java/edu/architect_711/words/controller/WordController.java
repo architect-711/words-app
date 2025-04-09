66240e775e69f491e3cb37fbc8d1827424322b96
Dmitrii <jfjjjnqhqiriykkn@gmail.com>
2025-04-09 10:28:06 +0300

    fix!(DhmVXPFI): api fix 🤡

--- backend/src/main/java/edu/architect_711/words/controller/WordController.java
+++ backend/src/main/java/edu/architect_711/words/controller/WordController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/words")
+@RequestMapping("/api/words")
 @RequiredArgsConstructor
 public class WordController {
     private final DefaultWordService defaultWordService;
