51c6df003f208ec7a18adba09c5c211b7e7258f5
Dmitrii <jfjjjnqhqiriykkn@gmail.com>
2025-04-14 00:26:34 +0300

    feat(DhmVXPFI): add profile-depend filler

---
+++ backend/src/main/java/edu/architect_711/words/onstart/WordsFiller.java
@@ -0,0 +1,33 @@
+package edu.architect_711.words.onstart;
+
+import edu.architect_711.words.controller.service.WordService;
+import edu.architect_711.words.entities.dto.WordDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Component @RequiredArgsConstructor @Profile("dev")
+public class WordsFiller implements ApplicationRunner {
+    private final WordService service;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (Objects.requireNonNull(service.read(20, 0).getBody()).size() == 20)
+            return;
+
+        for (byte i = 0; i < 20; i++)
+            service.create(new WordDto(
+                    null,
+                    "title" + LocalDateTime.now(),
+                    "translation",
+                    "description",
+                    "Russian",
+                    null
+            ));
+    }
+}
