f60f3aabb84ebbe7a73c5f432021102ceb5fbb6a
Dmitrii <jfjjjnqhqiriykkn@gmail.com>
2025-04-15 10:44:52 +0300

    fix(DhmVXPFI): throw out to particular controller

--- backend/src/main/java/edu/architect_711/words/controller/WordsFrontendController.java
+++ backend/src/main/java/edu/architect_711/words/controller/WordsFrontendController.java
@@ -4,7 +4,6 @@ import edu.architect_711.words.controller.service.LanguageService;
 import edu.architect_711.words.controller.service.WordService;
 import edu.architect_711.words.entities.db.WordEntity;
 import edu.architect_711.words.entities.dto.WordDto;
-import edu.architect_711.words.entities.mapper.WordMapper;
 import edu.architect_711.words.repository.WordRepository;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
@@ -17,38 +16,25 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@Controller
+@Controller @RequiredArgsConstructor @RequestMapping("/words")
 @Validated
-@RequiredArgsConstructor
-public class FrontendController {
-    private static final WordMapper wordMapper = new WordMapper();
-
+public class WordsFrontendController {
     private final WordService wordService;
     private final WordRepository wordRepository;
     private final LanguageService languageService;
 
     @GetMapping
-    public String index() {
-        return "index";
+    public String words() {
+        return "words";
     }
 
-    @PostMapping(path = "/save_new", consumes = {"application/x-www-form-urlencoded"})
-    public String saveNew(@RequestParam Map<String, String> map) {
-        wordService.create(new WordDto(null, map.get("title"), map.get("translation"), map.get("description"), map.get("language"), null));
 
-        return "redirect:/words";
-    }
 
-    @GetMapping("/new")
-    public String newOne(Model model) {
-        model.addAttribute("word", new WordDto());
-        model.addAttribute("langs", languageService.findAll().getBody());
-
-        return "new_word";
-    }
-
-    @GetMapping("/words")
-    public String words(
+    /* ------------------------------------------------- */
+    /*                  FIND WORDS                       */
+    /* ------------------------------------------------- */
+    @GetMapping("/find")
+    public String find(
             @RequestParam(defaultValue = "5", name = "size") int size,
             @RequestParam(defaultValue = "0", name = "page") int page,
             Model model
@@ -57,10 +43,10 @@ public class FrontendController {
         model.addAttribute("size", size);
         model.addAttribute("page", page);
 
-        return "words";
+        return "find_words";
     }
 
-    @PostMapping(path = "/word", consumes = {"application/x-www-form-urlencoded"})
+    @PostMapping(path = "/find", consumes = {"application/x-www-form-urlencoded"})
     public String findByTitle(
             @RequestParam Map<String, String> map,
             Model model
@@ -69,34 +55,79 @@ public class FrontendController {
         model.addAttribute("size", 5);
         model.addAttribute("page", 0);
 
-        return "words";
+        return "find_words";
     }
 
+
+
+    /* ------------------------------------------------- */
+    /*                  CREATE WORDS                     */
+    /* ------------------------------------------------- */
+    @GetMapping("/new")
+    public String newOne(Model model) {
+        model.addAttribute("word", new WordDto());
+        model.addAttribute("langs", languageService.findAll().getBody());
+
+        return "new_word";
+    }
+
+    @PostMapping(path = "/new", consumes = {"application/x-www-form-urlencoded"})
+    public String saveNew(@RequestParam Map<String, String> map) {
+        wordService.create(new WordDto(
+                null,
+                map.get("title"),
+                map.get("translation"),
+                map.get("description"),
+                map.get("language"),
+                null
+        ));
+
+        return "redirect:/words";
+    }
+
+
+
+
+    /* ------------------------------------------------- */
+    /*                   UPDATE WORDS                    */
+    /* ------------------------------------------------- */
     @PostMapping("/update")
     @Validated(value = {Default.class, WordDto.OnCreated.class})
     public String update(@Valid WordDto word, BindingResult result, Model model) {
         if (result.hasErrors())
-            result.failOnError(IllegalArgumentException::new);
+            result.failOnError(RuntimeException::new);
 
         wordService.update(word);
 
         return "redirect:/words";
     }
 
-    @GetMapping("/words/{id}")
+
+
+
+    /* ------------------------------------------------- */
+    /*                      WORD CARD                    */
+    /* ------------------------------------------------- */
+    @GetMapping("/{id}")
     public String getById(@PathVariable Long id, Model model) {
         final WordEntity entity = wordRepository.safeFindWordById(id);
 
         model.addAttribute("word", entity);
+        model.addAttribute("langs", languageService.findAll().getBody());
 
         return "word_card";
     }
 
+
+
+
+    /* ------------------------------------------------- */
+    /*                   DELETE WORDS                    */
+    /* ------------------------------------------------- */
     @GetMapping("/delete/{id}")
     public String deleteById(@PathVariable Long id, Model model) {
         wordRepository.deleteById(id);
 
         return "redirect:/words";
     }
-
 }
