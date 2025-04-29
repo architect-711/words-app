8a7f84e7ad15b36841462df52472cf899cdeb673
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-04-29 18:14:30 +0300

    build: update
    it finally works properly!
    
    (cherry picked from commit eafa56c46b326891fab909c1706de6a7642e9906)

--- scripts/run-spring-prod.sh
+++ scripts/run-spring-prod.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Make sure you run all build commands before!"
+echo "--> !!! Make sure you run all build commands before !!!"
 
+# import tasty junk
 source utils.sh
+# populate with env vars
 source_prod
 
 docker compose up -d postgres_prod_db
