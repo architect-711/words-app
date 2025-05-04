0c38c0beb265587e7c9b77872adda03fb8560f34
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-04 20:13:06 +0300

    fix!: mb it works finally - params based words fetch

--- frontend/src/pages/WordsPage.tsx
+++ frontend/src/pages/WordsPage.tsx
@@ -6,6 +6,7 @@ import { fetchLanguages, fetchWords, find } from "../api/fetchers";
 import WordContainer from "../components/words/WordsContainer";
 import { buildFallbackMessage } from "../utils/error";
 import { createSearchParams, useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { getParams } from "../utils/params";
 
 const defaultParams : WordsPagFetchParams = { page : 0 , size : 10, lang: '', title: ''};
 
@@ -15,57 +16,35 @@ export default function WordsPage() {
 
     const [fetchErrors, setFetchErrors] = useState<FetchError[]>([]);
 
-    const {page, size, lang, title}     = useParams();
-
-    const [params, setParams]           = useState<WordsPagFetchParams>(defaultParams);
-
-    const nav = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const {page, size, lang, title}     = getParams(searchParams);
 
     const handleError = (error : FetchError) : void => setFetchErrors([ ...fetchErrors, error ]);
 
-    const onPrev = () => setParams({ ...params, page : --params.page });
-    const onNext = () => setParams({ ...params, page : ++params.page });
-
-    const onFind = (input : string, langSelect : string) : void => {
-        setParams({ ...params, title : input, lang : langSelect});
-        const pars = {
-            page : params.page.toString(),
-            size : params.size.toString(),
-            lang : langSelect,
-            title : input
-        }
-        nav({
-            pathname : '/words',
-            search : `?${createSearchParams(pars)}`
-        });
-
+    const fetchWords = () : void => {
         find(
-            e => setFetchErrors([...fetchErrors, e]), 
-            r => setWords(r.data), 
-            params.page, 
-            params.size, 
-            input, 
-            langSelect
+            handleError,
+            r => setWords(r.data),
+            page,
+            size,
+            title,
+            lang
         );
     }
 
     useEffect(() => {
-        onFind(params.title, params.lang);
+        fetchWords();
         fetchLanguages(handleError, r => setLanguages(r.data)); 
-
-        return () => setFetchErrors([]);
-    }, []);
+    }, [page]);
 
     return (
         <div className="content mt-20">
 
-            <Search languages={languages} onFind={onFind}/>
+            <Search languages={languages}/>
             {fetchErrors == null || fetchErrors.length <= 0
                 ? <WordContainer 
                     words={words} 
-                    onNext={onNext} 
-                    onPrev={onPrev}
-                    disablePrev={params.page <= 0}
+                    disablePrev={(page ?? 0) <= 0}
                 />
                 : <div>
                     {
