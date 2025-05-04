74ddac8cee488828027002477c71a82bd3de82db
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-04 20:12:06 +0300

    fix!: navigation with params

--- frontend/src/components/nav_buttons/NavButtons.tsx
+++ frontend/src/components/nav_buttons/NavButtons.tsx
@@ -1,26 +1,49 @@
+import { Link, useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import styles from './NavButtons.module.css';
+import { WordsPagFetchParams } from '../../types/global';
+import { getParams } from '../../utils/params';
 
 interface Params {
-    onPrev : () => void,
-    onNext : () => void,
     disablePrev? : boolean
     disableNext? : boolean
 }
 
-function NavButtons({ onPrev, onNext, disableNext = false, disablePrev = false } : Params) {
+type LinkDir = "next" | "prev";
+
+function NavButtons({ disableNext = false, disablePrev = false } : Params) {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const {page, size, lang, title} = getParams(searchParams);
+
+    const navigate = (type : LinkDir) : void => {
+        let p = page ?? 0;
+
+        if (type == 'next') p += 1;
+        else p -= 1;
+
+        const params = {
+            title : title ?? '',
+            lang : lang ?? '',
+            page : String(p),
+            size : String(size)
+        };
+
+        setSearchParams(params);
+    }
+
     return (
         <div className={`${styles.container} pag-buttons mt-10`}>
-            <button 
+            <button
+                onClick={() => navigate('prev')}
                 type="button" 
-                onClick={onPrev}
                 className={`${styles.button} btn btn-regular`}
-                disabled={disablePrev}
+                style={{ display : disablePrev ? 'none' : 'initial' }}
             >Prev</button>
-            <button 
+            <button
+                onClick={() => navigate('next')}
                 type="button" 
-                onClick={onNext}
                 className={`${styles.button} btn btn-regular`}
-                disabled={disableNext}
+                style={{ display : disableNext ? 'none' : 'initial' }}
             >Next</button>
         </div>
     );
