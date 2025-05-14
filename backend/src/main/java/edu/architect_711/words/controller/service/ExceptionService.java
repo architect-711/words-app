0ada3dd13c640eddeb80e4da3801664dba142203
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-14 23:37:35 +0300

    chore: throw out all stupid Uncle Bob's interfaces

--- backend/src/main/java/edu/architect_711/words/controller/service/ExceptionService.java
+++
@@ -1,9 +0,0 @@
-package edu.architect_711.words.controller.service;
-
-import org.springframework.http.ResponseEntity;
-
-import edu.architect_711.words.entities.dto.ExceptionResponseDto;
-
-public interface ExceptionService {
-    ResponseEntity<ExceptionResponseDto> exception(Exception exception);
-}
--- backend/src/main/java/edu/architect_711/words/entities/Language.java
+++
@@ -1,5 +0,0 @@
-package edu.architect_711.words.entities;
-
-public interface Language {
-    String getTitle();
-}
