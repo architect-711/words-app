30e4773780507014d84a1b45d12162124174dffa
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:33:13 +0300

    refactor: update everything, looks better and safer

--- backend/src/main/java/edu/architect_711/words/service/group/DefaultWordGroupService.java
+++ backend/src/main/java/edu/architect_711/words/service/group/DefaultWordGroupService.java
@@ -1,86 +1,75 @@
 package edu.architect_711.words.service.group;
 
-import edu.architect_711.words.controller.service.GroupService;
 import edu.architect_711.words.entities.db.GroupEntity;
 import edu.architect_711.words.entities.dto.GroupDto;
-import edu.architect_711.words.entities.mapper.EntityMapper;
-import edu.architect_711.words.entities.mapper.GroupMapper;
 import edu.architect_711.words.repository.GroupRepository;
-import edu.architect_711.words.service.OffsetCalculator;
+import edu.architect_711.words.service.word.WordService;
+import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.groups.Default;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Optional;
+
+import static edu.architect_711.words.entities.mapper.GroupMapper.toDto;
+import static edu.architect_711.words.entities.mapper.GroupMapper.toEntity;
+import static edu.architect_711.words.service.OffsetCalculator.regular;
 
 @Service @RequiredArgsConstructor
 @Validated
 public class DefaultWordGroupService implements GroupService {
     private final GroupRepository groupRepository;
-    private GroupRepository wordGroupRepository;
-
-    private static final EntityMapper<GroupEntity, GroupDto> mapper = new GroupMapper();
-
+    private final WordService wordService;
+    
     @Override
-    public ResponseEntity<GroupDto> getAll() {
-        return null;
+    public @NonNull List<GroupDto> getAll(@NonNull Integer size, @NonNull Integer page) {
+        return toDto(groupRepository.findAll(PageRequest.of(page, size)).getContent()); // TODO do you remember the problems the PageRequest's brought?
     }
 
     @Override
     @Validated({Default.class, GroupDto.OnCreate.class})
-    public ResponseEntity<GroupDto> save(@Valid GroupDto groupDto) {
-        return buildOk( mapToDto( groupRepository.save(mapper.toEntity(groupDto) ) ) );
+    public @NonNull GroupDto save(@Valid GroupDto groupDto) {
+        final List<Long> wordsIds = groupDto.getWordsIds();
+
+        if (wordsIds != null && !wordsIds.isEmpty()) {
+            wordsIds.forEach(wordId -> Optional.ofNullable(wordService.getById(wordId)).orElseThrow(() -> new IllegalArgumentException("Word with id " + wordId + " not found")));
+        }
+        
+        return toDto(groupRepository.save(toEntity(groupDto)));
     }
 
-    @Override @Validated(value = {Default.class, GroupDto.OnCreated.class})
-    public ResponseEntity<GroupDto> update(GroupDto groupDto) {
+    @Override
+    @Validated(value = {Default.class, GroupDto.OnCreated.class})
+    public @NonNull GroupDto update(@Valid GroupDto groupDto) {
         final GroupEntity groupEntity = groupRepository.safeFindById(groupDto.getId());
 
         groupEntity.setDescription(groupDto.getDescription());
         groupEntity.setTitle(groupDto.getTitle());
         groupEntity.setWordsIds(groupDto.getWordsIds());
 
-        return buildOk( mapToDto( groupRepository.save(groupEntity) ) );
+        return toDto(groupRepository.save(groupEntity));
     }
 
     @Override
-    public ResponseEntity<GroupDto> delete(Long id) {
-        return null;
+    public void deleteById(@NonNull Long id) {
+        groupRepository.deleteById(id);
+    }
+
+    @Nullable
+    @Override
+    public GroupDto getById(@NonNull Long id) {
+        return toDto(groupRepository.safeFindById(id));
     }
 
     @Override
-    public ResponseEntity<GroupDto> getById(Long id) {
-        return buildOk( mapToDto( groupRepository.safeFindById(id) ) );
-    }
+    public @NonNull List<Long> getWordIds(@NonNull Long id, @NonNull Integer size, @NonNull Integer page) {
+        groupRepository.safeFindById(id);
 
-    @Override
-    public ResponseEntity<Set<Long>> getByIdWords(final Long id, final Integer size, final Integer page) {
-        final long OFFSET = OffsetCalculator.regular((long) size, (long) page);
-        final Set<Long> wordsIds = groupRepository.findPaginatedWordsIds(id, size, OFFSET);
-
-        return ResponseEntity.ok( wordsIds );
-    }
-
-    @Override
-    public ResponseEntity<List<GroupDto>> getAllGroups(@Min(value = 0) Integer size, @Min(value = 0) Integer page) {
-        return buildOk( mapToDto( groupRepository.paginatedFind((long) size, OffsetCalculator.regular((long) size, (long) page)) ) );
-    }
-
-    private static GroupDto mapToDto(GroupEntity entity) {
-        return mapToDto( List.of(entity)).getFirst() ;
-    }
-
-    private static List<GroupDto> mapToDto(List<GroupEntity> groups) {
-        return groups.stream().map(mapper::toDto).collect(Collectors.toList());
-    }
-
-    private static <T> ResponseEntity<T> buildOk(T t) {
-        return ResponseEntity.ok(t);
+        return groupRepository.findPaginatedWordsIds(id, size, regular(size, page));
     }
 }
