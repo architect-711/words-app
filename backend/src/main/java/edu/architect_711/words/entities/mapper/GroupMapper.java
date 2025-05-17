e214bfebe4768eaeeb41f91df2bce38f7b1227f2
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:31:50 +0300

    feat: add `toDto` method for lists and all are static

--- backend/src/main/java/edu/architect_711/words/entities/mapper/GroupMapper.java
+++ backend/src/main/java/edu/architect_711/words/entities/mapper/GroupMapper.java
@@ -3,10 +3,12 @@ package edu.architect_711.words.entities.mapper;
 import edu.architect_711.words.entities.db.GroupEntity;
 import edu.architect_711.words.entities.dto.GroupDto;
 
-public class GroupMapper implements EntityMapper<GroupEntity, GroupDto> {
+import java.util.List;
+import java.util.stream.Collectors;
 
-    @Override
-    public GroupDto toDto(GroupEntity entity) {
+public class GroupMapper {
+
+    public static GroupDto toDto(GroupEntity entity) {
         return new GroupDto(
                 entity.getId(),
                 entity.getTitle(),
@@ -16,13 +18,16 @@ public class GroupMapper implements EntityMapper<GroupEntity, GroupDto> {
         );
     }
 
-    @Override
-    public GroupEntity toEntity(GroupDto dto) {
+    public static GroupEntity toEntity(GroupDto dto) {
         return new GroupEntity(
                 dto.getTitle(),
                 dto.getDescription(),
                 dto.getWordsIds(),
                 dto.getCreated()
         );
+    }
+
+    public static List<GroupDto> toDto(List<GroupEntity> entities) {
+        return entities.stream().map(GroupMapper::toDto).collect(Collectors.toList());
     }
 }
