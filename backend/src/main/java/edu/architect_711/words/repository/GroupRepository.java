7db70e350ab1e0bec2dbf1b9d2e1c60869115487
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:32:19 +0300

    feat: add many useful methods for service

--- backend/src/main/java/edu/architect_711/words/repository/GroupRepository.java
+++ backend/src/main/java/edu/architect_711/words/repository/GroupRepository.java
@@ -4,27 +4,14 @@ import edu.architect_711.words.entities.db.GroupEntity;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Set;
 
 public interface GroupRepository extends JpaRepository<GroupEntity, Long> {
     default GroupEntity safeFindById(Long id) {
         return findById(id).orElseThrow(() -> new EntityNotFoundException("Group not found with id: " + id));
     }
-
-    @Query(
-            nativeQuery = true,
-            value = """
-            select * from
-                word_groups
-            limit
-                :size
-            offset
-                :offset
-            """
-    )
-    List<GroupEntity> paginatedFind(final Long size, final Long offset);
 
     @Query(
             nativeQuery = true,
@@ -41,5 +28,22 @@ public interface GroupRepository extends JpaRepository<GroupEntity, Long> {
                 :offset
             """
     )
-    Set<Long> findPaginatedWordsIds(long id, Integer size, long offset);
+    List<Long> findPaginatedWordsIds(long id, Integer size, long offset);
+
+    @Query(
+            nativeQuery = true,
+            value = """
+            select
+                *
+            from
+                word_groups
+            where
+                words_ids is not null
+            limit
+                :size
+            offset
+                :offset
+            """
+    )
+    List<GroupEntity> findAllWhereWordsIdsNotNull(@Param("size") Integer size, @Param("offset") Integer offset);
 }
