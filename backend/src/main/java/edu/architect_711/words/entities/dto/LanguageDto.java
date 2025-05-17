ecc8dcedfc040a6e8e89a57ca8e246f57d4e31dd
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:23:57 +0300

    feat: add `id` to the lang dto

--- backend/src/main/java/edu/architect_711/words/entities/dto/LanguageDto.java
+++ backend/src/main/java/edu/architect_711/words/entities/dto/LanguageDto.java
@@ -1,13 +1,17 @@
 package edu.architect_711.words.entities.dto;
 
-import edu.architect_711.words.entities.Language;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data @AllArgsConstructor @NoArgsConstructor
-public class LanguageDto implements Language {
+public class LanguageDto {
+    @NonNull @Min(0)
+    private Long id;
+
     @NotBlank(message = "Title cannot be blank")
     private String title;
 }
