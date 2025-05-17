19793a959a1857e0b3ff02e13ba1079dcaa65847
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:39:19 +0300

    fix: update words filler

--- backend/src/main/java/edu/architect_711/words/onstart/WordsFiller.java
+++ backend/src/main/java/edu/architect_711/words/onstart/WordsFiller.java
@@ -1,8 +1,9 @@
 package edu.architect_711.words.onstart;
 
-import edu.architect_711.words.controller.service.WordService;
 import edu.architect_711.words.entities.dto.WordDto;
+import edu.architect_711.words.service.word.DefaultWordService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
@@ -10,26 +11,36 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
-@Component @RequiredArgsConstructor @Profile("dev")
+@Component @RequiredArgsConstructor @Profile("dev") @Slf4j
 public class WordsFiller implements ApplicationRunner {
-    private final WordService service;
+    private final DefaultWordService service;
+
+    private final static int LIMIT = 5;
+
+    private static WordDto GET_DUMMY_WORD() {
+        return new WordDto(
+                null,
+                "title" + LocalDateTime.now(),
+                List.of("translation"),
+                "description",
+                List.of(),
+                "Russian",
+                null,
+                "some transcription"
+        );
+    }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if (Objects.requireNonNull(service.read(20, 0).getBody()).size() >= 5)
+        if (service.getAll(20, 0).size() >= 5) {
+            log.debug("There is enough words, exiting filler");
             return;
+        }
 
-        for (byte i = 0; i < 5; i++)
-            service.create(new WordDto(
-                    null,
-                    "title" + LocalDateTime.now(),
-                    "translation",
-                    "description",
-                    List.of(),
-                    "Russian",
-                    null
-            ));
+        log.debug("Filling words, will be added: " + LIMIT);
+
+        for (byte i = 0; i < LIMIT; i++)
+            service.save(GET_DUMMY_WORD());
     }
 }
