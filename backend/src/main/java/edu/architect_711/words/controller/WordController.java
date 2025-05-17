5f7a45dea995599b833ba5f516aa97230c974b21
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:42:24 +0300

    refactor: controller does only HTTP related things

--- backend/src/main/java/edu/architect_711/words/controller/WordController.java
+++ backend/src/main/java/edu/architect_711/words/controller/WordController.java
@@ -1,8 +1,10 @@
 package edu.architect_711.words.controller;
 
+import edu.architect_711.words.entities.dto.BaseGroupDto;
 import edu.architect_711.words.entities.dto.WordDto;
 import edu.architect_711.words.service.word.DefaultWordService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,44 +18,59 @@ public class WordController {
     private final DefaultWordService defaultWordService;
 
     @GetMapping
-    public ResponseEntity<List<WordDto>> read(
+    public ResponseEntity<List<WordDto>> getAll(
             @RequestParam(name = "size", defaultValue = "5") Integer size,
             @RequestParam(name = "page", defaultValue = "0") Integer page
     ) {
-        return defaultWordService.read(size, page);
+        return ResponseEntity.ok(defaultWordService.getAll(page, size));
     }
 
     @GetMapping("/find")
-    public ResponseEntity<List<WordDto>> find(
+    public ResponseEntity<List<WordDto>> search(
         @RequestParam(name = "size", defaultValue = "10") Integer size,
         @RequestParam(name = "page", defaultValue = "0") Integer page,
         @RequestParam(name = "title", defaultValue = "") String title,
         @RequestParam(name = "lang", defaultValue = "") String lang
     ) {
-        return defaultWordService.find(size, page, title, lang);
+        return ResponseEntity.ok(defaultWordService.search(size, page, title, lang));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<WordDto> getById(
             @PathVariable("id") Long id
     ) {
-        return defaultWordService.getById(id);
+        return ok(defaultWordService.getById(id));
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteMany(@RequestBody List<Long> ids) {
+        defaultWordService.delete(ids);
+    }
+
+    @GetMapping("/{id}/groups")
+    public ResponseEntity<List<BaseGroupDto>> getGroupTitles(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(defaultWordService.getGroupsTitles(id));
     }
 
     @PostMapping
     public ResponseEntity<WordDto> create(@RequestBody WordDto wordDto) {
-        return defaultWordService.create(wordDto);
+        return ok(defaultWordService.save(wordDto));
     }
 
     @PutMapping
     public ResponseEntity<WordDto> update(@RequestBody WordDto wordDto) {
-        return defaultWordService.update(wordDto);
+        return ok(defaultWordService.update(wordDto));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
-        return defaultWordService.delete(id);
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("id") Long id) {
+        defaultWordService.delete(id);
     }
-    
+
+    private static ResponseEntity<WordDto> ok(WordDto payload) {
+        return ResponseEntity.ok(payload);
+    }
 }
 
