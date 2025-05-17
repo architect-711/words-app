80be2205cce57f0ea7418560334c1d16e71e692d
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:39:34 +0300

    feat: add many useful methods

--- backend/src/main/java/edu/architect_711/words/repository/WordRepository.java
+++ backend/src/main/java/edu/architect_711/words/repository/WordRepository.java
@@ -1,55 +1,77 @@
 package edu.architect_711.words.repository;
 
 import edu.architect_711.words.entities.db.WordEntity;
+import edu.architect_711.words.entities.dto.BaseGroupDto;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface WordRepository extends JpaRepository<WordEntity, Long> {
-    Optional<WordEntity> findByTitle(final String title);
+    @Query(
+            nativeQuery = true,
+            value = """
+            select * from word
+            limit :size
+            offset :offset
+            """
+    )
+    List<WordEntity> findAllPaginated(@Param("size") int size, @Param("offset") int offset);
 
     @Query(
             nativeQuery = true,
             value = """
-                    SELECT * FROM word
-                    WHERE title LIKE %:title%
-                    LIMIT :size OFFSET :page
-                    """
+            select * from word
+            where title like '%:title%'
+            limit :size
+            offset :offset
+            """
     )
-    List<WordEntity> findByTitleApproximates(final String title, final int size, final int page);
+    List<WordEntity> findByTitleApproximates(@Param("title") String title, @Param("size") int size, @Param("offset") int offset);
 
-    default WordEntity safeFindWordById(Long id) {
+    default WordEntity safeFindWordById(Long id) throws EntityNotFoundException {
         return findById(id).orElseThrow(() -> new EntityNotFoundException("WordEntity not found with id: " + id));
     }
 
-    @Query(nativeQuery = true, value = """
-            SELECT * FROM word
-            WHERE account_id = :id
-            LIMIT :limit OFFSET :offset
-            """)
-    List<WordEntity> findAllPaginatedById(Long id, Long limit, Long offset);
-// TODO fix naming, page -> offset
     @Query(
             nativeQuery = true, value = """
-            SELECT word.id,
+            select word.id,
                    word.title,
                    word.translation,
                    word.description,
                    word.language_id,
                    word.use_cases,
                    word.local_date_time
-            FROM word
-            INNER JOIN language
-            ON word.language_id = language.id
-            WHERE language.title LIKE %:lang%
-            LIMIT :size OFFSET :page;
+            from
+                word
+            inner join
+                language
+            on
+                word.language_id = language.id
+            where
+                language.title like '%:lang%'
+            limit
+                :size
+            offset
+                :offset;
             """
     )
-    List<WordEntity> findPaginatedByLangAprx(@Param("lang") String lang, 
-               @Param("size") int size, @Param("page") int page);
+    List<WordEntity> findPaginatedByLangApproximately(@Param("lang") String lang,
+                                                      @Param("size") int size, @Param("offset") int offset);
+
+    @Query(
+            nativeQuery = true,
+            value = """
+            select
+                id, title
+            from
+                word_groups
+            where
+                :id = any (words_ids)
+            """
+    )
+    List<BaseGroupDto> findWordGroupTitles(@Param("id") Long wordId);
 
 }
