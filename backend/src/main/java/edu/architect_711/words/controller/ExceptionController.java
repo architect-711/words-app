c620b00ce109f78fc76f96df6c43eeb2418852da
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:37:42 +0300

    refactor: update to stand with the Service

--- backend/src/main/java/edu/architect_711/words/controller/ExceptionController.java
+++ backend/src/main/java/edu/architect_711/words/controller/ExceptionController.java
@@ -1,19 +1,21 @@
 package edu.architect_711.words.controller;
 
+import edu.architect_711.words.entities.dto.ExceptionResponseDto;
+import edu.architect_711.words.service.exception.ContainerBasedExceptionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import edu.architect_711.words.controller.service.ExceptionService;
-import edu.architect_711.words.entities.dto.ExceptionResponseDto;
-import lombok.RequiredArgsConstructor;
-
 @RestControllerAdvice @RequiredArgsConstructor
 public class ExceptionController {
-    private final ExceptionService exceptionService;
+    private final ContainerBasedExceptionService exceptionService;
 
     @ExceptionHandler(Exception.class) 
     public ResponseEntity<ExceptionResponseDto> exception(Exception ex) {
-        return exceptionService.exception(ex);
+        final ExceptionResponseDto response = exceptionService.handle(ex);
+
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(response.getStatus()));
     }
 }
