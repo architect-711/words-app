0a759de860342e99ceb242faa91e80548717dcb5
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:33:43 +0300

    refactor: update according to the Service

--- backend/src/main/java/edu/architect_711/words/controller/GroupController.java
+++ backend/src/main/java/edu/architect_711/words/controller/GroupController.java
@@ -1,48 +1,59 @@
 package edu.architect_711.words.controller;
 
-import edu.architect_711.words.controller.service.GroupService;
 import edu.architect_711.words.entities.dto.GroupDto;
+import edu.architect_711.words.service.group.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Set;
 
 @RestController  @RequestMapping("/api/groups")
 @RequiredArgsConstructor
+@CrossOrigin
 public class GroupController {
     private final GroupService groupService;
 
     @GetMapping
     public ResponseEntity<List<GroupDto>> getAllGroups(
-            @RequestParam(value = "size", defaultValue = "10") Integer size,
-            @RequestParam(value = "page", defaultValue = "0") Integer page
+            @RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
+            @RequestParam(value = "page", defaultValue = "0", required = false) Integer page
     ) {
-        return groupService.getAllGroups(size, page);
+        return ResponseEntity.ok(groupService.getAll(size, page));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<GroupDto> getGroupById(@PathVariable(name = "id") Long id) {
-        return groupService.getById(id);
+        return ok(groupService.getById(id));
     }
 
     @PostMapping
     public ResponseEntity<GroupDto> createGroup(@RequestBody GroupDto groupDto) {
-        return groupService.save(groupDto);
+        return ok(groupService.save(groupDto));
     }
 
     @PutMapping
     public ResponseEntity<GroupDto> updateGroup(@RequestBody GroupDto groupDto) {
-        return groupService.update(groupDto);
+        return ok(groupService.update(groupDto));
     }
 
     @GetMapping("/{id}/words")
-    public ResponseEntity<Set<Long>> getGroupWords(
+    public ResponseEntity<List<Long>> getGroupWords(
             @PathVariable(name = "id") Long id,
             @RequestParam(defaultValue = "10", required = false) Integer size,
             @RequestParam(defaultValue = "0", required = false) Integer page
     ) {
-        return groupService.getByIdWords(id, size, page);
+        return ResponseEntity.ok(groupService.getWordIds(id, size, page));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        groupService.deleteById(id);
+
+        return ResponseEntity.ok().build();
+    }
+
+    private static ResponseEntity<GroupDto> ok(final GroupDto dto) {
+        return ResponseEntity.ok(dto);
     }
 }
