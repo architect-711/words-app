672072b8ecc0763cc21ca7b7fb7e8605c3d864ca
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:37:21 +0300

    refactor: make err service look like service

--- backend/src/main/java/edu/architect_711/words/service/exception/ContainerBasedExceptionService.java
+++ backend/src/main/java/edu/architect_711/words/service/exception/ContainerBasedExceptionService.java
@@ -1,13 +1,10 @@
 package edu.architect_711.words.service.exception;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
 import edu.architect_711.words.entities.dto.ExceptionDescription;
 import edu.architect_711.words.entities.dto.ExceptionResponseDto;
-import edu.architect_711.words.controller.service.ExceptionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Service @RequiredArgsConstructor @Slf4j
 public class ContainerBasedExceptionService implements ExceptionService {
@@ -15,23 +12,23 @@ public class ContainerBasedExceptionService implements ExceptionService {
     private final ExceptionResponseBuilder responseBuilder;
 
     @Override
-    public ResponseEntity<ExceptionResponseDto> exception(Exception exception) {
-        log.error("Cought the exception: ", exception);
+    public ExceptionResponseDto handle(Exception exception) {
+        log.error("Caught the exception: ", exception);
 
         if (!(exception instanceof RuntimeException runtimeException))
             return defaultResponse(exception);
 
-        ExceptionDescription foundException = exceptionContainer.findByClass(runtimeException.getClass());
+        final ExceptionDescription foundException = exceptionContainer.findByClass(runtimeException.getClass());
         return foundException == null
                 ? defaultResponse(new Exception(runtimeException))
                 : runtimeResponse(foundException, runtimeException);
     }
 
-    private ResponseEntity<ExceptionResponseDto> defaultResponse(Exception exception) {
-        return ResponseEntity.badRequest().body(responseBuilder.buildDefault(exception));
+    private ExceptionResponseDto defaultResponse(Exception exception) {
+        return responseBuilder.buildDefault(exception);
     }
 
-    private ResponseEntity<ExceptionResponseDto> runtimeResponse(ExceptionDescription ed, RuntimeException re) {
-        return ResponseEntity.status(ed.getStatus()).body(responseBuilder.buildDto(ed, re.getMessage()));
+    private ExceptionResponseDto runtimeResponse(ExceptionDescription ed, RuntimeException re) {
+        return responseBuilder.buildDto(ed, re.getMessage());
     }
 }
