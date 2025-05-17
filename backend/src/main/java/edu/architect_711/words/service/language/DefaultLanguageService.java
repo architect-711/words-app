123223e9405667efa7e30bfa300b4dc2030726b3
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:28:24 +0300

    fix!: lang service looks like a service
    it doesn't care about HTTP stuff anymore

--- backend/src/main/java/edu/architect_711/words/service/language/DefaultLanguageService.java
+++ backend/src/main/java/edu/architect_711/words/service/language/DefaultLanguageService.java
@@ -1,22 +1,19 @@
 package edu.architect_711.words.service.language;
 
 import edu.architect_711.words.entities.dto.LanguageDto;
-import edu.architect_711.words.entities.mapper.LanguageMapper;
 import edu.architect_711.words.repository.LanguageRepository;
-import edu.architect_711.words.controller.service.LanguageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static edu.architect_711.words.entities.mapper.LanguageMapper.toDto;
 
 @Service @RequiredArgsConstructor
 public class DefaultLanguageService implements LanguageService {
     private final LanguageRepository languageRepository;
 
-    public ResponseEntity<List<LanguageDto>> findAll() {
-        LanguageMapper languageMapper = new LanguageMapper();
-
-        return ResponseEntity.ok(languageRepository.findAll().stream().map(languageMapper::toDto).toList());
+    public List<LanguageDto> findAll() {
+        return toDto(languageRepository.findAll());
     }
 }
