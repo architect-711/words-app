d9235a46d5737aeeafbdab44f98dae377547fac2
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 00:16:20 +0300

    chore: delete not used custom exceptions

--- backend/src/main/java/edu/architect_711/words/exception/custom/MethodNotImplementedException.java
+++
@@ -1,7 +0,0 @@
-package edu.architect_711.words.exception.custom;
-
-public class MethodNotImplementedException extends RuntimeException {
-    public MethodNotImplementedException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/edu/architect_711/words/exception/custom/TokenNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package edu.architect_711.words.exception.custom;
-
-public class TokenNotFoundException extends RuntimeException {
-    public TokenNotFoundException(String message) {
-        super(message);
-    }
-}
