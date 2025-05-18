8a1368aea2db104033234526da0e05446b66b275
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-05-18 22:29:02 +0300

    feat: refactor and new reverse method

--- backend/src/main/java/edu/architect_711/words/service/OffsetCalculator.java
+++ backend/src/main/java/edu/architect_711/words/service/OffsetCalculator.java
@@ -1,7 +1,11 @@
 package edu.architect_711.words.service;
 
 public class OffsetCalculator {
-    public static int regular(final int size, final int page) {
+    public static int offset(final int size, final int page) {
         return size * page;
+    }
+
+    public static int page(final int offset, final int size) {
+        return offset / size;
     }
 }
